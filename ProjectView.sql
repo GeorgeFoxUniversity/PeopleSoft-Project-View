2155a60a64dac60698751fb0aaeb3dbbef5d0d95
Ryan Backman <ryan@ryanbackman.net>
2012-08-01 09:29:33 -0700

    Reformated and added ProjectView as Field

--- ProjectView.sql
+++ ProjectView.sql
@@ -1,160 +1,161 @@
-select
-case PI.OBJECTTYPE
-  when 0 then 'Records'
-  when 1 then 'Indexes'
-  when 2 then 'Fields'
-  when 3 then 'Field Formats'
-  when 4 then 'Translate Values'
-  when 5 then 'Pages'
-  when 6 then 'Menus'
-  when 7 then 'Components'
-  when 8 then 'Record PeopleCode'
-  when 9 then 'Menu PeopleCode'
-  when 10 then 'Queries'
-  when 11 then 'Tree Structures'
-  when 12 then 'Trees'
-  when 13 then 'Access Groups'
-  when 14 then 'Colors'
-  when 15 then 'Styles'
-  when 16 then 'Business Process Maps'
-  when 17 then 'Business Processes'
-  when 18 then 'Activities'
-  when 19 then 'Roles'
-  when 20 then 'Process Definitions'
-  when 21 then 'Servers Definitions'
-  when 22 then 'Process Type Definitions'
-  when 23 then 'Job Definitions'
-  when 24 then 'Recurrence Definitions'
-  when 25 then 'Message Catalog' -- ??
-  when 26 then 'Dimensions'
-  when 27 then 'Analysis Models'
-  when 28 then 'Cube Templates'
-  when 29 then 'Business Interlink'
-  when 30 then 'SQL'
-  when 31 then 'File Layout Definitions'
-  when 32 then 'Component Interfaces'
-  when 33 then 'Application Engine Programs'
-  when 34 then 'Application Engine Sections'
-  when 35 then 'Message Nodes'
-  when 36 then 'Message Channels'
-  when 37 then 'Messages'
-  when 38 then 'Approval Rule Sets'
-  when 39 then 'Message PeopleCode'
-  when 40 then 'Subscription PeopleCode'
-  when 41 then 'Channel PeopleCode'
-  when 42 then 'Component Interface PeopleCode'
-  when 43 then 'Application Engine PeopleCode'
-  when 44 then 'Page PeopleCode'
-  when 45 then 'Page Field PeopleCode'
-  when 46 then 'Component PeopleCode'
-  when 47 then 'Component Record PeopleCode'
-  when 48 then 'Component Record Field PeopleCode'
-  when 49 then 'Images'
-  when 50 then 'Style Sheets'
-  when 51 then 'HTML'
-  when 52 then 'File References'
-  when 53 then 'Permission Lists'
-  when 54 then 'Portal Registry Definitions'
-  when 55 then 'Portal Registry Structures'
-  when 56 then 'URL Definitions'
-  when 57 then 'Application Packages'
-  when 58 then 'Application Package PeopleCode'
-  when 59 then 'Portal Registry User Homepage'
-  when 60 then 'Analytic Types'
-  when 61 then 'Archive Template'
-  when 62 then 'XSLT'
-  when 63 then 'Portal Registry User Favorite'
-  when 64 then 'Mobile Page'
-  when 65 then 'Relationship'
-  when 66 then 'CI Property PeopleCode'
-  when 67 then 'Optimization Model'
-  when 68 then 'File Reference'
-  when 69 then 'File Reference Type Code'
-  when 70 then 'Archive Object'
-  when 71 then 'Archive Template (Type 2)'
-  when 72 then 'Diagnostic Plug-In'
-  when 73 then 'Analytic Model'
-  when 75 then 'Java Portlet User Preference'
-  when 76 then 'WSRP Remote Producer'
-  when 77 then 'WSRP Remote Portlet'
-  when 78 then 'WSRP Cloned Portlet Handle'
-  when 79 then 'Service'
-  when 80 then 'Service Operation'
-  when 81 then 'Service Operation Handler'
-  when 82 then 'Service Operation Version'
-  when 83 then 'Service Operation Routing'
-  when 84 then 'IB Queue'
-  when 85 then 'XMLP Template Definition'
-  when 86 then 'XMLP Report Definition'
-  when 87 then 'XMLP File Definition'
-  when 88 then 'XMLP Data Source Definition'
-  when 89 then 'WSDL'
-  when 90 then 'Message Schema'
-  when 91 then 'Connected Query'
-  when 92 then 'Document'
-  when 93 then 'XML Document'
-  when 94 then 'Relational Document'
-  when 95 then 'Dependency Documents'
-  when 97 then 'Essbase Cube Dimension'
-  when 98 then 'Essbase Cube Outline'
-  when 99 then 'Essbase Cube Connection'
-  when 100 then 'Essbase Cube Template'
-  when 101 then 'Delimited Document'
-  when 102 then 'Positional Document'
-  when 103 then 'Application Data Set Definition'
-  when 104 then 'Tests'
-  when 105 then 'Test Cases'
-  else To_char(PI.OBJECTTYPE)
-end                                        as "Object Type",
-PI.OBJECTVALUE1                            as "Object Value 1",
-case PI.OBJECTID2
-  when 81 then case PI.OBJECTVALUE2
-                 when '0' then 'Normal'
-                 when '1' then 'Application Engine'
-                 when '2' then 'Record View'
-                 when '5' then 'Audit'
-                 when '6' then 'App Engine XSLT'
-                 else PI.OBJECTVALUE2
-               end
-  when 100 then case PI.OBJECTVALUE2
-                  when 'C' then 'Content Ref.'
-                  when 'F' then 'Folder'
-                  when 'V' then 'Favorite'
-                  else PI.OBJECTVALUE2
-                end
-  else PI.OBJECTVALUE2
-end                                        as "Object Value 2",
-PI.OBJECTVALUE3                            as "Object Value 3",
-PI.OBJECTVALUE4                            as "Object Value 4",
-Decode(PI.SOURCESTATUS, 0, 'Unknown',
-                        1, 'Absent',
-                        2, 'Changed',
-                        3, 'Unchanged',
-                        4, '*Changed',
-                        5, '*Unchanged',
-                        6, 'Same',
-                        PI.SOURCESTATUS)   as "Source",
-Decode(PI.TARGETSTATUS, 0, 'Unknown',
-                        1, 'Absent',
-                        2, 'Changed',
-                        3, 'Unchanged',
-                        4, '*Changed',
-                        5, '*Unchanged',
-                        6, 'Same',
-                        PI.TARGETSTATUS)   as "Target",
-Decode(PI.UPGRADEACTION, 0, 'Copy',
-                         1, 'Delete',
-                         2, 'None',
-                         3, 'CopyProp',
-                         PI.UPGRADEACTION) as "Action",
-Decode(PI.TAKEACTION, 0, 'No',
-                      1, 'Yes',
-                      PI.TAKEACTION)       as "Take Action"
-from   PSPROJECTITEM PI
-where  PI.PROJECTNAME in ( 'DLP_COMPARE' )
--- AND  (PI.SOURCESTATUS IN (4,5) or PI.TARGETSTATUS IN (4,5))
-order  by PI.OBJECTTYPE,
-          PI.OBJECTVALUE1,
-          PI.OBJECTVALUE2,
-          PI.OBJECTVALUE3,
-          PI.OBJECTVALUE4; 
+SELECT PI.projectname, 
+       CASE PI.objecttype 
+         WHEN 0 THEN 'Records' 
+         WHEN 1 THEN 'Indexes' 
+         WHEN 2 THEN 'Fields' 
+         WHEN 3 THEN 'Field Formats' 
+         WHEN 4 THEN 'Translate Values' 
+         WHEN 5 THEN 'Pages' 
+         WHEN 6 THEN 'Menus' 
+         WHEN 7 THEN 'Components' 
+         WHEN 8 THEN 'Record PeopleCode' 
+         WHEN 9 THEN 'Menu PeopleCode' 
+         WHEN 10 THEN 'Queries' 
+         WHEN 11 THEN 'Tree Structures' 
+         WHEN 12 THEN 'Trees' 
+         WHEN 13 THEN 'Access Groups' 
+         WHEN 14 THEN 'Colors' 
+         WHEN 15 THEN 'Styles' 
+         WHEN 16 THEN 'Business Process Maps' 
+         WHEN 17 THEN 'Business Processes' 
+         WHEN 18 THEN 'Activities' 
+         WHEN 19 THEN 'Roles' 
+         WHEN 20 THEN 'Process Definitions' 
+         WHEN 21 THEN 'Servers Definitions' 
+         WHEN 22 THEN 'Process Type Definitions' 
+         WHEN 23 THEN 'Job Definitions' 
+         WHEN 24 THEN 'Recurrence Definitions' 
+         WHEN 25 THEN 'Message Catalog' -- ?? 
+         WHEN 26 THEN 'Dimensions' 
+         WHEN 27 THEN 'Analysis Models' 
+         WHEN 28 THEN 'Cube Templates' 
+         WHEN 29 THEN 'Business Interlink' 
+         WHEN 30 THEN 'SQL' 
+         WHEN 31 THEN 'File Layout Definitions' 
+         WHEN 32 THEN 'Component Interfaces' 
+         WHEN 33 THEN 'Application Engine Programs' 
+         WHEN 34 THEN 'Application Engine Sections' 
+         WHEN 35 THEN 'Message Nodes' 
+         WHEN 36 THEN 'Message Channels' 
+         WHEN 37 THEN 'Messages' 
+         WHEN 38 THEN 'Approval Rule Sets' 
+         WHEN 39 THEN 'Message PeopleCode' 
+         WHEN 40 THEN 'Subscription PeopleCode' 
+         WHEN 41 THEN 'Channel PeopleCode' 
+         WHEN 42 THEN 'Component Interface PeopleCode' 
+         WHEN 43 THEN 'Application Engine PeopleCode' 
+         WHEN 44 THEN 'Page PeopleCode' 
+         WHEN 45 THEN 'Page Field PeopleCode' 
+         WHEN 46 THEN 'Component PeopleCode' 
+         WHEN 47 THEN 'Component Record PeopleCode' 
+         WHEN 48 THEN 'Component Record Field PeopleCode' 
+         WHEN 49 THEN 'Images' 
+         WHEN 50 THEN 'Style Sheets' 
+         WHEN 51 THEN 'HTML' 
+         WHEN 52 THEN 'File References' 
+         WHEN 53 THEN 'Permission Lists' 
+         WHEN 54 THEN 'Portal Registry Definitions' 
+         WHEN 55 THEN 'Portal Registry Structures' 
+         WHEN 56 THEN 'URL Definitions' 
+         WHEN 57 THEN 'Application Packages' 
+         WHEN 58 THEN 'Application Package PeopleCode' 
+         WHEN 59 THEN 'Portal Registry User Homepage' 
+         WHEN 60 THEN 'Analytic Types' 
+         WHEN 61 THEN 'Archive Template' 
+         WHEN 62 THEN 'XSLT' 
+         WHEN 63 THEN 'Portal Registry User Favorite' 
+         WHEN 64 THEN 'Mobile Page' 
+         WHEN 65 THEN 'Relationship' 
+         WHEN 66 THEN 'CI Property PeopleCode' 
+         WHEN 67 THEN 'Optimization Model' 
+         WHEN 68 THEN 'File Reference' 
+         WHEN 69 THEN 'File Reference Type Code' 
+         WHEN 70 THEN 'Archive Object' 
+         WHEN 71 THEN 'Archive Template (Type 2)' 
+         WHEN 72 THEN 'Diagnostic Plug-In' 
+         WHEN 73 THEN 'Analytic Model' 
+         WHEN 75 THEN 'Java Portlet User Preference' 
+         WHEN 76 THEN 'WSRP Remote Producer' 
+         WHEN 77 THEN 'WSRP Remote Portlet' 
+         WHEN 78 THEN 'WSRP Cloned Portlet Handle' 
+         WHEN 79 THEN 'Service' 
+         WHEN 80 THEN 'Service Operation' 
+         WHEN 81 THEN 'Service Operation Handler' 
+         WHEN 82 THEN 'Service Operation Version' 
+         WHEN 83 THEN 'Service Operation Routing' 
+         WHEN 84 THEN 'IB Queue' 
+         WHEN 85 THEN 'XMLP Template Definition' 
+         WHEN 86 THEN 'XMLP Report Definition' 
+         WHEN 87 THEN 'XMLP File Definition' 
+         WHEN 88 THEN 'XMLP Data Source Definition' 
+         WHEN 89 THEN 'WSDL' 
+         WHEN 90 THEN 'Message Schema' 
+         WHEN 91 THEN 'Connected Query' 
+         WHEN 92 THEN 'Document' 
+         WHEN 93 THEN 'XML Document' 
+         WHEN 94 THEN 'Relational Document' 
+         WHEN 95 THEN 'Dependency Documents' 
+         WHEN 97 THEN 'Essbase Cube Dimension' 
+         WHEN 98 THEN 'Essbase Cube Outline' 
+         WHEN 99 THEN 'Essbase Cube Connection' 
+         WHEN 100 THEN 'Essbase Cube Template' 
+         WHEN 101 THEN 'Delimited Document' 
+         WHEN 102 THEN 'Positional Document' 
+         WHEN 103 THEN 'Application Data Set Definition' 
+         WHEN 104 THEN 'Tests' 
+         WHEN 105 THEN 'Test Cases' 
+         ELSE To_char(PI.objecttype) 
+       END                                        AS "Object Type", 
+       PI.objectvalue1                            AS "Object Value 1", 
+       CASE PI.objectid2 
+         WHEN 81 THEN 
+           CASE PI.objectvalue2 
+             WHEN '0' THEN 'Normal' 
+             WHEN '1' THEN 'Application Engine' 
+             WHEN '2' THEN 'Record View' 
+             WHEN '5' THEN 'Audit' 
+             WHEN '6' THEN 'App Engine XSLT' 
+             ELSE PI.objectvalue2 
+           END 
+         WHEN 100 THEN 
+           CASE PI.objectvalue2 
+             WHEN 'C' THEN 'Content Ref.' 
+             WHEN 'F' THEN 'Folder' 
+             WHEN 'V' THEN 'Favorite' 
+             ELSE PI.objectvalue2 
+           END 
+         ELSE PI.objectvalue2 
+       END                                        AS "Object Value 2", 
+       PI.objectvalue3                            AS "Object Value 3", 
+       PI.objectvalue4                            AS "Object Value 4", 
+       Decode(PI.sourcestatus, 0, 'Unknown', 
+                               1, 'Absent', 
+                               2, 'Changed', 
+                               3, 'Unchanged', 
+                               4, '*Changed', 
+                               5, '*Unchanged', 
+                               6, 'Same', 
+                               PI.sourcestatus)   AS "Source", 
+       Decode(PI.targetstatus, 0, 'Unknown', 
+                               1, 'Absent', 
+                               2, 'Changed', 
+                               3, 'Unchanged', 
+                               4, '*Changed', 
+                               5, '*Unchanged', 
+                               6, 'Same', 
+                               PI.targetstatus)   AS "Target", 
+       Decode(PI.upgradeaction, 0, 'Copy', 
+                                1, 'Delete', 
+                                2, 'None', 
+                                3, 'CopyProp', 
+                                PI.upgradeaction) AS "Action", 
+       Decode(PI.takeaction, 0, 'No', 
+                             1, 'Yes', 
+                             PI.takeaction)       AS "Take Action" 
+FROM   psprojectitem PI 
+ORDER  BY PI.projectname, 
+          PI.objecttype, 
+          PI.objectvalue1, 
+          PI.objectvalue2, 
+          PI.objectvalue3, 
+          PI.objectvalue4; 
